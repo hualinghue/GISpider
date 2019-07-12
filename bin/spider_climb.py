8f39fdad7111729bfb97f4236b0574a70fb82889
hualinghue <liang3633152@gmai.com>
2019-07-12 09:06:02 +0800

    asd

--- bin/spider_climb.py
+++ bin/spider_climb.py
@@ -8,12 +8,10 @@ from core import GetImgAddress,customize_class
 import inspect
 
 if __name__ == "__main__":
-    pool = Pool(4)
+    pool = Pool(8)
     for name, obj in inspect.getmembers(customize_class):  #遍历所有自定义采集信息对象
         if inspect.isclass(obj) and name!='Options' and obj.display:     #筛选符合的对象
-            print(1)
-            bb = GetImgAddress.DriveEngine(obj())
-            pool.apply_async(bb.run)
+            pool.apply_async(GetImgAddress.DriveEngine,(obj(),))
     print('---start---')
     pool.close()  # 关闭线程池，关闭后不再接受进的请求
     pool.join()#等待进程池所有进程都执行完毕后，开始执行下面语句
